fc7c420efd7c94bdb890224b0c39d22da1f7eebf
David Cole <davidtcole@yahoo.com>
2020-06-05 16:01:44 -0700

    finally get my spaghetti code running for testing purposes. Forgot to commit regularly during a fair bit of work. A better description is that I added cards for my programming languages - images/descriptions/links, and added code in jquery to toggle the various form elements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,38 @@
 $(document).ready(function(){
-  $("#form-button").click(function(event){
-    $("#user-info").show();
-
+  $("#form-reveal").click(function(event){
     event.preventDefault();
+
+    $("#user-info").show();
   });
 
-  $("#form-submit").submit(function(event){
+  $("#user-info").submit(function(event){
     event.preventDefault();
 
     const firstNameInput = $("input#first-name").val();
     const lastNameInput = $("input#last-name").val();
-    const questionOneInput = $("input:radio[name=question1]:checked").val();
-    const questionTwoInput = $("input:radio[name=question2]:checked").val();
-    const questionThreeInput = $("input:radio[name=question3]:checked").val();
+    const questionOneInput = $('input:radio[name="question1"]:checked').val();
+    const questionTwoInput = $('input:radio[name="question2"]:checked').val();
+    const questionThreeInput = $('input:radio[name="question3"]:checked').val();
+    
+    $(".user-name").text(firstNameInput);
+    $("#user-info").hide();
+    $("#results").show();
+
+    determineLanguage(questionOneInput, questionTwoInput, questionThreeInput);
   });
 ;})
-
 
 function determineLanguage(questionOneInput, questionTwoInput, questionThreeInput){
 
   if((questionOneInput === "ruby") || (questionTwoInput === "ruby") || (questionThreeInput === "ruby") && (questionOneInput === questionTwoInput) 
   || (questionOneInput === questionThreeInput) || (questionTwoInput === questionThreeInput)){
-    //display ruby block
+    $("#ruby-card").toggle();
+
   } else if ((questionOneInput === "java") || (questionTwoInput === "java") || (questionThreeInput === "java") && (questionOneInput === questionTwoInput) 
   || (questionOneInput === questionThreeInput) || (questionTwoInput === questionThreeInput)){
-    //display Java block
+    $("#java-card").toggle();
   } else if ((questionOneInput === "js") || (questionTwoInput === "js") || (questionThreeInput === "js") && (questionOneInput === questionTwoInput) 
   || (questionOneInput === questionThreeInput) || (questionTwoInput === questionThreeInput)){
-    //display JavaScript block
+    $("#javascript-card").toggle();
   }
 }
